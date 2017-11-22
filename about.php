94f449f3ce11c00608b496424c6d3e05f8b37a63
Marc Williams <oti_instructor@hotmail.com>
2017-11-22 08:16:35 -0400

    Initial Commit

---
+++ about.php
@@ -0,0 +1,5 @@
+<?php
+    include 'includes/header.php';
+    include 'templates/about.php';
+    include 'includes/footer.php';
+
---
+++ templates/about.php
@@ -0,0 +1,30 @@
+        <!-- Page Page Content -->
+        <div class="container">
+            <!-- mwilliams:  Page Header/Breadcrumbs -->
+            <h1 class="mt-4 mb-3">About
+                <small>Our Business</small>
+            </h1>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                    <a href="index.html">Home</a>
+                </li>
+                <li class="breadcrumb-item active">About</li>
+            </ol>
+            <!-- End Page Header/Breadcrumbs -->
+            <div class="row">
+                <div class="col-lg-6">
+                    <img class="img-fluid rounded mb-4" src="http://placehold.it/750x450" alt="">
+                </div>
+                <div class="col-lg-6">
+                    <h2>About Our Business</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed voluptate nihil eum consectetur similique? Consectetur, quod, incidunt, harum nisi dolores delectus reprehenderit voluptatem perferendis dicta dolorem non blanditiis ex fugiat.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, magni, aperiam vitae illum voluptatum aut sequi impedit non velit ab ea pariatur sint quidem corporis eveniet. Odit, temporibus reprehenderit dolorum!</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, consequuntur, modi mollitia corporis ipsa voluptate corrupti eum ratione ex ea praesentium quibusdam? Aut, in eum facere corrupti necessitatibus perspiciatis quis?</p>
+                </div>
+            </div>
+            <!-- /.row -->
+
+
+        </div>
+        <!-- /.container -->
+
